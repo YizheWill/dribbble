d6b9c6fc9cc14267a8bd95ab814ee9ec6c229626
Yizhe Wang <yizhe87@gmail.com>
2020-12-01 23:34:10 -0800

    schema file changed

--- dribbble_api/db/schema.rb
+++ dribbble_api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_02_045858) do
+ActiveRecord::Schema.define(version: 2020_12_02_070119) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -92,6 +92,7 @@ ActiveRecord::Schema.define(version: 2020_12_02_045858) do
     t.boolean "work_availability", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "avatar_url"
   end
 
   add_foreign_key "collections", "users"
