9127dfd5ccf475c09be01ae96e3925ad8c6493c0
Yizhe Wang <yizhe87@gmail.com>
2020-12-10 11:16:55 -0800

    backend logics

--- willwill/app/javascript/react/reducer/UiReducer.js
+++
@@ -1,8 +0,0 @@
-const _initState = {};
-export default (preState = _initState, action) => {
-  Object.freeze(preState);
-  switch (action.type) {
-    default:
-      return preState;
-  }
-};
--- willwill/commands/commands.sh
+++
@@ -1,7 +0,0 @@
-rails new willwill --skip-turbolinks -d postgresql
-rake webpacker:install:react
-# make changes to files in app/javascript/hello_react.jsx
-# create app/javascript/react/App.js components...
-rails g controller Welcome index
-
-
